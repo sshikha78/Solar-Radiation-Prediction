e2325390d21b83b6b1e164488faebdc221c21023
sshikha78 <sharma07@gwu.edu>
2023-03-26 00:32:44 -0400

    update

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 from statsmodels.tsa.seasonal import STL
 import Tool
 import numpy as np
-url = ''
-df = pd.read_csv(url, index_col='Date',)
-date = pd.date_range(start = '1981-01-01',
-                    periods = len(df),
-                    freq='D')
+
+url = 'https://raw.githubusercontent.com/sshikha78/Solar-Radiation-Prediction/main/SolarPrediction.csv'
+df = pd.read_csv(url)
+print(df)
+
+
