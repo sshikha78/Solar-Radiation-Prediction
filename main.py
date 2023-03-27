205370eb1eeafaddaf4a6590f910e03360f9d5b2
sshikha78 <sharma07@gwu.edu>
2023-03-27 14:33:52 -0400

    update

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ import Tool
 import numpy as np
 
 url = 'https://raw.githubusercontent.com/sshikha78/Solar-Radiation-Prediction/main/SolarPrediction.csv'
-df = pd.read_csv(url)
-print(df)
+df = pd.read_csv(url,index_col = 'Data')
+date = pd.date_range(start = '2016-09-29',
+                    periods = len(df),
+                    freq='D')
+df.index = date
+df_hourly = df.resample('H').mean()
+df_hourly.to_csv('SolarPrediction_hourly.csv')
+
+print(df.shape)
+print(df.info())
+
+
 
 
